dba4fce1f8613c2b511712dbc5343c2e64cdf34e
ArunThomas-S <thoma394@gmail.com>
2025-11-01 09:53:00 +0530

    first commit

---
+++ JS/task1.js
@@ -0,0 +1,17 @@
+let a=10
+let b=20
+let c=35
+document.writeln("<br/>The sum of a and b is: " + (a + b));
+document.writeln("<br/>The difference of a and b is: " + (a - b));
+document.writeln("<br/>The product of a and b is: " + (a * b));
+document.writeln("<br/>The quotient of a and b is: " + (a / b));
+document.writeln("<br/>The remainder of a and b is: " + (a % b));
+document.writeln("<br/>The average of the three numbers is: " + ((a + b + c) / 3));
+document.writeln("<br/>The area of the rectangle is:" + (a * b));
+F=(c*9/5)+32
+document.writeln("<br/>The temperature in Fahrenheit is: " + F);
+document.writeln("<br/>The remainder of c and a is: " + (c % a));
+document.writeln("<br/>The value of a after increment by 10 is: " + (a +=10));
+document.writeln("<br/>The value of b after decrement by 5 is: " + (b -=5));
+document.writeln("<br/>The value of c after multiplying by 3 is: " + (c *=3));
+document.writeln("<br/>The value of a after dividing by 2 is: " + (a /=2));
